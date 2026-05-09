97b8cfff2482a47336af13d6fee88d1addbe3705
saoufi sami <saoufi-sami@hotmail.fr>
2026-05-09 22:30:52 +0200

    my first method

--- programme_collections/exercice_count.py
+++ programme_collections/exercice_count.py
@@ -3,3 +3,15 @@
 
 #         0         1        2          3         4      5
 noms = ["Jean", "Sophie","Martin","Christophe","Zoe","Martin"]
+
+
+# compter tout les caracteres ex jean 4 + sophie = 10 ..
+# premier methode boucle for + len
+
+total = 0
+
+for nom in noms:
+    nombre = len(nom)
+    total += nombre
+
+print(total)
